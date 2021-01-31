f6e50a3918fa7c50d6dd386b53bea43689300a99
sowndarya13 <78270581+sowndarya13@users.noreply.github.com>
2021-01-31 14:51:46 +0530

    Create accept and add two num.c

---
+++ accept and add two num.c
@@ -0,0 +1,11 @@
+//get values from user and print their addition
+#include <stdio.h>
+
+int main()
+{
+   int a,b;
+   printf("enter two nos. for which addition has to be done:\n");
+   scanf("%d%d",&a,&b);
+   printf("the addition of %d and %d is %d",a,b,a+b);
+   
+}
