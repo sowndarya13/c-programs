445758c2e11b939a7f531484751275f076b4d652
sowndarya13 <78270581+sowndarya13@users.noreply.github.com>
2021-01-31 13:29:29 +0530

    Create sqrt using sqrt().c

---
+++ sqrt using sqrt().c
@@ -0,0 +1,11 @@
+//squareroot of a number using sqrt()
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    printf("enter a number:");
+    scanf("%d",&n);
+    printf("square root of %d is %lf\n",n,sqrt(n));
+    return 0;
+}
